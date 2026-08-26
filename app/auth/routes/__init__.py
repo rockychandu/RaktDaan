c894228ad406f5a2711058962bc70593a936a1d6
rockychandu <rockychandu2005@gmail.com>
2026-08-26 12:32:27 +0530

    Member1

--- app/auth/routes/__init__.py
+++ app/auth/routes/__init__.py
@@ -4,5 +4,6 @@ Auth Routes Blueprint Package
 from app.auth.routes.donor_routes import donor_bp
 from app.auth.routes.admin_routes import admin_bp
 from app.auth.routes.health_routes import health_bp
+from app.auth.routes.ui_routes import ui_bp
 
-__all__ = ["donor_bp", "admin_bp", "health_bp"]
+__all__ = ["donor_bp", "admin_bp", "health_bp", "ui_bp"]
